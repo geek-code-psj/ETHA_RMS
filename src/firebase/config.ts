67cbda9917a4d9bdf1a001aefd14413c12572007
PRABAL SINGH <email.prabalsingh1@gmail.com>
2026-03-13 16:39:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3304326559-4b36e",
+  "appId": "1:452309717463:web:cf1aa164f08f6c6d0ffb92",
+  "apiKey": "AIzaSyBxQSSsf43jdSvb5SrbmuO2fBHVpN6n8OQ",
+  "authDomain": "studio-3304326559-4b36e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "452309717463"
+};
